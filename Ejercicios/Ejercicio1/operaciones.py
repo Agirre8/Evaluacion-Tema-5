dad42adf478ff0f229aaaacecd1e1f96ba4fb8bb
Agirre8 <inagi30@gmail.com>
2022-12-05 20:15:28 +0100

    operacion suma

---
+++ Ejercicios/Ejercicio1/operaciones.py
@@ -0,0 +1,15 @@
+def suma():
+    num1 = int(input("Introduzca el primer número de la suma"))#Tengo que especificar que num1 es un int sino en la suma me quedaria por ejemplo 3+7=37
+    num2 = int(input("Introduzca el segundo número de la suma"))
+    resultado = num1 + num2
+
+    return print(f"El resultado de la suma es:{resultado}")
+
+def resta(self, num1, num2):
+    return
+def producto(self, num1, num2):
+    return
+def division(self, num1, num2):
+    return
+
+suma()
