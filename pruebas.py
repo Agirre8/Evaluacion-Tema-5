c96f4c92e159213a61019af60d7a83be2b4a659d
Agirre8 <inagi30@gmail.com>
2022-12-06 14:04:40 +0100

    +

--- pruebas.py
+++ pruebas.py
@@ -1,6 +1,23 @@
-from  tkinter import *
-import datetime
 
-def obtener_hora_actual():
-    return datetime.now().strftime("%H:%M:%S")
+from tkinter import *
+from tkinter.ttk import *
+
+from time import strftime
+
+raiz = Tk()
+raiz.title('Clock')
+
+def hora_actual():
+    string = strftime('%H:%M:%S %p')
+    lbl.config(text=string)
+    lbl.after(1000, hora_actual)
+ 
+
+lbl = Label(raiz)
+ 
+
+lbl.pack(anchor='center')
+hora_actual()
+ 
+mainloop()
 
