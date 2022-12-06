b88ec519374e68c3bc4b80822042db58f227bc6e
Agirre8 <inagi30@gmail.com>
2022-12-06 20:07:24 +0100

    cambios

--- Ejercicios/Ejercicio4/reloj.py
+++ Ejercicios/Ejercicio4/reloj.py
@@ -3,16 +3,16 @@ from tkinter.ttk import *
 from time import strftime
 
 raiz = Tk()#le añado a la variable raiz todas las funciones de la libreria tkinter
-raiz.title('Reloj')#creamos una pestaña llamada reloj con la funcion tittle
+raiz.title("Reloj")#creamos una pestaña llamada reloj con la funcion tittle
 
 
 def hora_actual():
-    hora = strftime('%H:%M:%S %p')#%H para horas M para minutos y S para segundos
+    hora = strftime("%H:%M:%S %p")#%H para horas M para minutos y S para segundos
     lbl.config(text=hora)
     lbl.after(1000, hora_actual)#pongo 1000 para recargar la figura del reloj cada 1 segundos
 
 lbl = Label(raiz)#defino lbl
-lbl.pack(centro='center')
+lbl.pack(anchor="center")
 
 hora_actual()
 mainloop()#ejecuta en bucle el programa 
