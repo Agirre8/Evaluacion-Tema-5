f9cc9101ad650426ab8510d6bd0ca7cd7fd8469f
Agirre8 <inagi30@gmail.com>
2022-12-06 14:13:57 +0100

    ejercicio reloj con tkinter

--- pruebas.py
+++ pruebas.py
@@ -1,23 +1,18 @@
-
-from tkinter import *
+from tkinter import*
 from tkinter.ttk import *
-
 from time import strftime
 
-raiz = Tk()
-raiz.title('Clock')
+raiz = Tk()#le añado a la variable raiz todas las funciones de la libreria tkinter
+raiz.title('Reloj')#creamos una pestaña llamada reloj con la funcion tittle
+
 
 def hora_actual():
-    string = strftime('%H:%M:%S %p')
-    lbl.config(text=string)
-    lbl.after(1000, hora_actual)
- 
+    hora = strftime('%H:%M:%S %p')#%H para horas M para minutos y S para segundos
+    lbl.config(text=hora)
+    lbl.after(1000, hora_actual)#pongo 1000 para recargar la figura del reloj cada 1 segundos
 
-lbl = Label(raiz)
- 
+lbl = Label(raiz)#defino lbl
+lbl.pack(centro='center')
 
-lbl.pack(anchor='center')
 hora_actual()
- 
-mainloop()
-
+mainloop()#ejecuta en bucle el programa 
