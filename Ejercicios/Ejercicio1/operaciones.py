07838a106b8dc2e9afff2234e732ee71b3e85d05
Agirre8 <inagi30@gmail.com>
2022-12-06 12:19:46 +0100

    MODULO OPERACIONES

--- Ejercicios/Ejercicio1/operaciones.py
+++ Ejercicios/Ejercicio1/operaciones.py
@@ -1,15 +1,37 @@
 def suma():
-    num1 = int(input("Introduzca el primer número de la suma"))#Tengo que especificar que num1 es un int sino en la suma me quedaria por ejemplo 3+7=37
-    num2 = int(input("Introduzca el segundo número de la suma"))
-    resultado = num1 + num2
-
+    try:
+        num1 = int(input("Introduzca el primer número de la suma"))#Tengo que especificar que num1 es un int sino en la suma me quedaria por ejemplo 3+7=37
+        num2 = int(input("Introduzca el segundo número de la suma"))
+        resultado = num1 + num2
+    except ValueError:
+        print("Tipo de dato no válido.")
     return print(f"El resultado de la suma es:{resultado}")
 
-def resta(self, num1, num2):
-    return
-def producto(self, num1, num2):
-    return
-def division(self, num1, num2):
-    return
+def resta():
+    try:
+        num1 = int(input("Introduzca el primer número de la suma"))#Tengo que especificar que num1 es un int sino en la suma me quedaria por ejemplo 3+7=37
+        num2 = int(input("Introduzca el segundo número de la suma"))
+        resultado = num1 - num2
+    except ValueError:
+        print("Tipo de dato no válido.")
+    return print(f"El resultado de la suma es:{resultado}")
 
-suma()
+def producto():
+    try:
+        num1 = int(input("Introduzca el primer número de la suma"))#Tengo que especificar que num1 es un int sino en la suma me quedaria por ejemplo 3+7=37
+        num2 = int(input("Introduzca el segundo número de la suma"))
+        resultado = num1 * num2
+    except ValueError:
+        print("Tipo de dato no válido.")
+    return print(f"El resultado de la suma es:{resultado}")
+
+def division():
+    try:
+        num1 = int(input("Introduzca el primer número de la suma"))#Tengo que especificar que num1 es un int sino en la suma me quedaria por ejemplo 3+7=37
+        num2 = int(input("Introduzca el segundo número de la suma"))
+        resultado = num1 + num2
+    except ValueError:
+        print("Tipo de dato no válido.")
+    except ZeroDivisionError:
+        print("No es posible dividir entre cero")
+    return print(f"El resultado de la suma es:{resultado}")
